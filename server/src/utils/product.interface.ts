1e96756837e54da870077ae6f247d5fa52af6882
Shriti Negi <shritinegi24@gmail.com>
2026-02-08 14:41:36 +0530

    feat: add product interface

---
+++ server/src/utils/product.interface.ts
@@ -0,0 +1,18 @@
+import { Document } from "mongoose";
+
+export interface Product{
+    productName: string;
+    category:string;
+    subCategory:string;
+    brand:string;
+    salePrice:number;
+    marketPrice:number;
+    type:string;
+    rating:number;
+    description:string;
+    stock:number;
+    image?:string;
+}
+
+export interface ProductDocument extends Document,Product{}
+   
