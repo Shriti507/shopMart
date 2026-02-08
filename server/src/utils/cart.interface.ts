d0869340bfc96333b8a97915b6497a775b2fa60c
Shriti Negi <shritinegi24@gmail.com>
2026-02-08 15:06:28 +0530

    feat: add cart interface

---
+++ server/src/utils/cart.interface.ts
@@ -0,0 +1,14 @@
+import { Document,Types } from "mongoose";
+
+export interface CartItem{
+    product:Types.ObjectId; 
+    quantity:number;
+}
+
+
+export interface CartDocument extends Document{
+    user:Types.ObjectId;
+    items:CartItem[];
+    totalPrice:number
+}
+   
