170c1869f711eb90636b71eba9e6dbb35d15f6bb
Shriti Negi <shritinegi24@gmail.com>
2026-02-08 21:03:43 +0530

    feat: add payment interface

---
+++ server/src/utils/payment.interface.ts
@@ -0,0 +1,11 @@
+import { Document, Types } from "mongoose";
+
+export interface Payment extends Document {
+    orderId:Types.ObjectId; // links to order
+    userId:Types.ObjectId; //links to user
+    amount:number;          
+    status: 'pending'|'completed'|'failed'|'refunded';
+    paymentMethod: 'UPI'|'COD'|'Card'|'NetBanking';
+    gatewayTransactionId?: string; 
+    gatewaySignature?: string;     
+}
