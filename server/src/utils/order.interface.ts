03d09a210969836b8afc3893b31c7bb54f69cadf
Shriti Negi <shritinegi24@gmail.com>
2026-02-08 16:57:23 +0530

    feat: add order interface

---
+++ server/src/utils/order.interface.ts
@@ -0,0 +1,21 @@
+import { Document, Types } from "mongoose";
+
+export interface Order {
+    product: Types.ObjectId; 
+    name:string;            
+    price:number;          
+    quantity:number;
+    image:string;           
+}
+
+export interface IOrder extends Document {
+    user:Types.ObjectId;
+    society:Types.ObjectId; // to group deliveries  
+    items:Order[];
+    totalAmount:number;
+    status: 'Pending'|'Confirmed'|'Out for Delivery'|'Delivered'|'Cancelled';
+    paymentMethod: 'COD'|'UPI';
+    paymentStatus: 'Pending'|'Paid'|'Failed';
+}
+
+export interface OrderDocument extends Document,Order{}
