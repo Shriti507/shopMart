8f267131c7a404a5f99c958a4b3951c6f9126e43
Shriti Negi <shritinegi24@gmail.com>
2026-02-14 10:25:58 +0530

    feat: modify authService

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -2,5 +2,14 @@ import { userModel } from "../models/user.schema";
 import { UserDocument,User } from "../utils/user.interface";
 
 export class AuthService{
+    async getDetails(id:string){
+        return await userModel.findById(id)
+
+    }
+    async createUser(userData:User):Promise<UserDocument>{
+        const newUser=await userModel.create(userData)
+        console.log("user created.")
+        return newUser
+    }
     
 }
