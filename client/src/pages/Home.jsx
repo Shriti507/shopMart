cab08fdbf1e0233a1166f3759ae74ef9dfce69b2
Shriti Negi <shritinegi24@gmail.com>
2026-02-22 06:40:32 +0530

    fix: add the navigation bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
 import Banner from '../components/home/Banner'
 import Footer from '../components/home/Footer'
 import Hero from '../components/home/Hero'
+import Navigation from '../components/Navigation'
 
 const Home = () => {
   return (
     <div>
         
         <Banner/>
+        <Navigation/>
         <Hero/>
         <Footer/>
         
