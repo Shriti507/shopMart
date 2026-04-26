b8cbd90dc28082e9a6bee30918b9a1b1b98eb5d2
Shriti Negi <shritinegi24@gmail.com>
2026-04-26 15:06:08 +0530

    fix: relax eslint rules

--- server/eslint.config.ts
+++ server/eslint.config.ts
@@ -13,8 +13,9 @@ export default [
       },
     },
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn"
     },
   },
 
